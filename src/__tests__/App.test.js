85cea60f194a4bf088880cd87d544ad33b37f3cf
amosmaundundu <amosmaundu5@gmail.com>
2022-09-20 11:55:44 +0300

    react-hooks-react-app-example

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,12 +4,12 @@ import { format } from "date-fns";
 import App from "../components/App";
 
 beforeEach(() => {
-  render(<App />);
+  render(<App/> );
 });
 
 test('should include "Now" in the header instead of a time', () => {
   expect(
-    screen.queryByText(format(new Date(), "MMMM do yyyy, h:mm:ss a"))
+    screen.queryByText( now)
   ).not.toBeInTheDocument();
   expect(screen.queryByText(/Now/g)).toBeInTheDocument();
 });
